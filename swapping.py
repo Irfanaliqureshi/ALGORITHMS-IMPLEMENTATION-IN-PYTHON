6a2cfc83095c6b499add233382905edeaaa73399
Irfan Ali <114615634+Irfanaliqureshi@users.noreply.github.com>
2024-05-18 00:30:54 +0500

    swapping.py

---
+++ swapping.py
@@ -0,0 +1,17 @@
+# Swapping with three variables
+a=10
+ b=5
+print("Before Swapping Variables :", a,b)
+temp=a
+ a=b
+ b=temp
+ print("After Swapping of Variables :",a,b)
+
+
+# swapping with two variables
+a=10
+b=5
+print("Before Swapping Variables :",a,b)
+a=a+b
+b=a-b
+print("After Swapping of Variables :",a,b)
